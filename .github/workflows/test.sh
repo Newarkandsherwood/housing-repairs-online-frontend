e8541c6496f119a6bb3c6886866915f12c6e1925
el <elena@madetech.com>
2022-02-03 11:44:07 +0000

    simplify setting matix

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,18 +1,13 @@
 files=(tests/cypress/integration/**/*)
- matrix=$((
-   echo '{ "containers" : ['
-   echo '"data_string",'
+ matrix=$(
     for key in "${!files[@]}"
     do
       comma=","
       if [ ${files[$key]} == ${files[${#files[@]}-1]} ]; then
         comma=""
       fi
-      echo "${files[$key]#*/*/*/*}${comma}"| sed 's/ /, /g'  | sed -r 's/^([^,]*)(,?)$/"\1"\2/'
+      echo "${files[$key]#*/*/*/*}${comma}" | sed -r 's/^([^,]*)(,?)$/"\1"\2/'
     done
-
-   echo " ]}"
- ) | jq -c .)
+ )
 echo $matrix
-echo $matrix | jq .
-echo "::set-output name=matrix::$matrix"
+echo "::set-output name=matrix::[$matrix]"
