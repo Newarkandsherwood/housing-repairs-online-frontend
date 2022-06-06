352927b33d01a74a149803442507374fc876cf74
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-06-06 15:59:53 +0100

    Disable Sentry

--- next.config.js
+++ next.config.js
@@ -24,10 +24,11 @@ const moduleExports =  {
   },
 };
 
-module.exports = withSentryConfig(moduleExports, {
+/*module.exports = withSentryConfig(moduleExports, {
   org: 'housing-repairs-online',
   project: 'housing-repairs-online-frontend',
   authToken: process.env.SENTRY_AUTH_TOKEN || process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
   dryRun: false,
   include: './.next'
-});
+});*/
+module.exports = moduleExports;
