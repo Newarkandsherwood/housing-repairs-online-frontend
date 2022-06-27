6727c3166008226fa3c5efdb59e48db5a19a29a5
jamiejordanmt <jamie.jordan@madetech.com>
2022-06-27 09:55:34 +0100

    Add terraform step Co-authored-by: MaysaKanoni <maysa@madetech.com> Co-authored-by: Bhavesh-Patel <bhavesh.patel@madetech.com>

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.10.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+terraform {
+  backend "azurerm" {}
+}
+
+data "terraform_remote_state" "state" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
+  }
+}
+
+resource "azurerm_static_site" "hro_frontend_test" {
+  name                = "hrofrontendtest"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "storage_account_name" {
+  type = string
+}
+variable "container_name" {
+  type = string
+}
+variable "resource_group_name" {
+  type = string
+}
+variable "resource_group_location" {
+  type = string
+}
+variable "key" {
+  type = string
+}
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
