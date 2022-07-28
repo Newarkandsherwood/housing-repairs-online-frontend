b3a64f229fde886a13cacd58b61ad66c0baa7ca2
maysakanoni <maysa@madetech.com>
2022-07-28 11:25:40 +0100

    remove floating header

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html>
+      <Html className="govuk-template">
         <Head>
           {/* eslint-disable-next-line @next/next/no-sync-scripts */}
           <script src="/js/all.js"></script>
           <link rel="shortcut icon" href="/static/favicon.ico" />
         </Head>
-        <body className={'js-enabled'}>
+        <body className={'js-enabled govuk-template__body'}>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
