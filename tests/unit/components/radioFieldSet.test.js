8c9b89cc36a27f55fcc2bb809848229bf6492556
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-08-17 16:12:09 +0100

    93 clean reused variables in radioFieldSet.test.js

--- tests/unit/components/radioFieldSet.test.js
+++ tests/unit/components/radioFieldSet.test.js
@@ -1,10 +1,22 @@
 import RadioFieldSet from '../../../compoments/radioFieldSet';
 import React from 'react';
-import {render, unmountComponentAtNode} from 'react-dom';
-import {act} from 'react-dom/test-utils';
-import {renderToStaticMarkup} from 'react-dom/server';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 let container = null;
+
+
+const fieldSetName = 'fieldSetName';
+const firstValue = 'first';
+const firstTitle = 'First';
+const secondValue = 'second';
+const secondTitle = 'Second';
+const mockCallBack = jest.fn();
+const errorText = 'Required input value missing';
+const errorSummaryTitle = 'There is a problem';
+const emptyInputErrorMessage = 'Please enter something';
+
 beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement('div');
@@ -19,88 +31,74 @@ afterEach(() => {
 });
 
 describe('radioFieldSet', () => {
-  test ('Displays title', () => {
+  test('Displays title', () => {
     const titleText = 'A Title'
-    act(()=> {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={''}
         options={[]}
         title={titleText}
-      />, container)});
+      />, container)
+    });
     expect(container.textContent).toContain(titleText);
   })
-  test ('Displays single option', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstTitle = 'First';
-    const firstValue = 'first';
-
-    act(()=> {
+  test('Displays single option', () => {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}]}
+        options={[{ value: firstValue, title: firstTitle }]}
         title={''}
-      />, container)});
+      />, container)
+    });
     expect(container.querySelector('fieldset').textContent).toContain(firstTitle);
     expect(container.querySelector(`#${fieldSetName}-0`).value).toBe(firstValue);
   })
-  test ('Displays multiple options', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    const secondValue = 'second';
-    const secondTitle = 'Second';
-    act(()=> {
+  test('Displays multiple options', () => {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}, {value: secondValue, title: secondTitle}]}
+        options={[{ value: firstValue, title: firstTitle }, { value: secondValue, title: secondTitle }]}
         title={''}
-      />, container)});
+      />, container)
+    });
     const fieldSetTextContent = container.querySelector('fieldset').textContent;
     expect(fieldSetTextContent).toContain(firstTitle);
     expect(fieldSetTextContent).toContain(secondTitle);
     expect(container.querySelector(`#${fieldSetName}-0`).value).toBe(firstValue);
     expect(container.querySelector(`#${fieldSetName}-1`).value).toBe(secondValue);
   })
-  test ('Correct option is checked when single option', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    act(()=> {
+  test('Correct option is checked when single option', () => {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}]}
+        options={[{ value: firstValue, title: firstTitle }]}
         title={''}
         checked={firstValue}
-      />, container)});
+      />, container)
+    });
     expect(container.querySelector(`#${fieldSetName}-0`).checked).toBeTruthy();
   })
-  test ('Correct option is checked when multiple options', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    const secondValue = 'second';
-    const secondTitle = 'Second';
-    act(()=> {
+  test('Correct option is checked when multiple options', () => {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}, {value: secondValue, title: secondTitle}]}
+        options={[{ value: firstValue, title: firstTitle }, { value: secondValue, title: secondTitle }]}
         title={''}
         checked={secondValue}
       />, container)
     });
     expect(container.querySelector(`#${fieldSetName}-1`).checked).toBeTruthy();
   })
-  test ('Displays before button content', () => {
-    const fieldSetName = 'fieldSetName';
+  test('Displays before button content', () => {
     const beforeButtonContent = <div>hint text</div>;
-    act(()=> {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
         options={[]}
         title={''}
@@ -109,12 +107,11 @@ describe('radioFieldSet', () => {
     });
     expect(container.querySelector('#before-button-content').innerHTML).toBe(renderToStaticMarkup(beforeButtonContent));
   })
-  test ('Displays hint text', () => {
-    const fieldSetName = 'fieldSetName';
+  test('Displays hint text', () => {
     const hintText = 'hint text';
-    act(()=> {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
         options={[]}
         title={''}
@@ -123,12 +120,11 @@ describe('radioFieldSet', () => {
     });
     expect(container.querySelector(`#hint-text-${fieldSetName}`).textContent).toBe(hintText);
   })
-  test ('Shows button text', () => {
-    const fieldSetName = 'fieldSetName';
+  test('Shows button text', () => {
     const buttonText = 'hint text';
-    act(()=> {
+    act(() => {
       render(<RadioFieldSet
-        onSubmit={()=>{}}
+        onSubmit={() => { }}
         name={fieldSetName}
         options={[]}
         title={''}
@@ -139,16 +135,11 @@ describe('radioFieldSet', () => {
   })
   describe('Final item divider', () => {
     test('Displays divider before final item', () => {
-      const fieldSetName = 'fieldSetName';
-      const firstValue = 'first';
-      const firstTitle = 'First';
-      const secondValue = 'second';
-      const secondTitle = 'Second';
       act(() => {
         render(<RadioFieldSet
-          onSubmit={() => {}}
+          onSubmit={() => { }}
           name={fieldSetName}
-          options={[{ value: firstValue, title: firstTitle }, { value: secondValue, title: secondTitle}]}
+          options={[{ value: firstValue, title: firstTitle }, { value: secondValue, title: secondTitle }]}
           title={''}
           orDivider={true}
         />, container)
@@ -156,16 +147,11 @@ describe('radioFieldSet', () => {
       expect(container.querySelector('#final-divider')).toBeDefined();
     })
     test('Do not display divider before final item', () => {
-      const fieldSetName = 'fieldSetName';
-      const firstValue = 'first';
-      const firstTitle = 'First';
-      const secondValue = 'second';
-      const secondTitle = 'Second';
       act(() => {
         render(<RadioFieldSet
-          onSubmit={() => {}}
+          onSubmit={() => { }}
           name={fieldSetName}
-          options={[{ value: firstValue, title: firstTitle }, { value: secondValue, title: secondTitle}]}
+          options={[{ value: firstValue, title: firstTitle }, { value: secondValue, title: secondTitle }]}
           title={''}
         />, container)
       });
@@ -173,59 +159,47 @@ describe('radioFieldSet', () => {
     })
   })
   test('Clicking \'Submit\' button calls \'onSubmit\' handler', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    const mockCallBack = jest.fn();
-
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}]}
+        options={[{ value: firstValue, title: firstTitle }]}
         title={''}
         checked={firstValue}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(mockCallBack.mock.calls.length).toEqual(1);
   })
   test('Clicking \'Submit\' button without selection shows error', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    const mockCallBack = jest.fn();
-    const errorText = 'Required input value missing';
-
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}]}
+        options={[{ value: firstValue, title: firstTitle }]}
         title={''}
         errorText={errorText}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(container.querySelector(`#${fieldSetName}-error`).textContent).toBe(errorText);
   })
   test('Conditionals are displayed', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
     const label = 'Please enter your email address';
     const type = 'email';
 
     act(() => {
       render(<RadioFieldSet
-        onSubmit={() => {}}
+        onSubmit={() => { }}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle, conditional: {
-          label: label,
-          type: type
-        }}]}
+        options={[{
+          value: firstValue, title: firstTitle, conditional: {
+            label: label,
+            type: type
+          }
+        }]}
         title={''}
         checked={firstValue}
       />, container)
@@ -234,181 +208,152 @@ describe('radioFieldSet', () => {
     expect(container.querySelector(`#${fieldSetName}-${firstValue}`).type).toBe(type);
   })
   test('Clicking \'Submit\' button without conditional value shows error', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
     const label = 'Please enter your information';
     const type = 'text';
-    const mockCallBack = jest.fn();
     const emptyConditionalInputErrorMessage = 'Enter your information';
 
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle, conditional: {
-          label: label,
-          type: type,
-          emptyInputErrorMessage: emptyConditionalInputErrorMessage
-        }}]}
+        options={[{
+          value: firstValue, title: firstTitle, conditional: {
+            label: label,
+            type: type,
+            emptyInputErrorMessage: emptyConditionalInputErrorMessage
+          }
+        }]}
         title={''}
         checked={firstValue}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(container.querySelector(`#${fieldSetName}-conditional-error`).textContent).toBe(emptyConditionalInputErrorMessage);
   })
   test('Clicking \'Submit\' button with conditional value does not show error ', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
     const label = 'Please enter your email address';
     const type = 'email';
-    const mockCallBack = jest.fn();
 
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle, conditional: {
-          label: label,
-          type: type
-        }}]}
+        options={[{
+          value: firstValue, title: firstTitle, conditional: {
+            label: label,
+            type: type
+          }
+        }]}
         title={''}
         checked={firstValue}
-        conditionalValue={{[firstValue]: 'me@test.com'}}
+        conditionalValue={{ [firstValue]: 'me@test.com' }}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(container.querySelector(`#${fieldSetName}-error`).textContent).toBe('');
   })
   test('Clicking \'Submit\' button with invalid conditional value shows error', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
     const label = 'Please enter your email address';
     const type = 'email';
-    const mockCallBack = jest.fn();
     const invalidConditionalInputErrorMessage = 'Invalid input';
     const conditionalInputValidator = {
-      isValid: () => {return false}
+      isValid: () => { return false }
     }
 
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle, conditional: {
-          label: label,
-          type: type,
-          validator: conditionalInputValidator,
-          invalidInputErrorMessage: invalidConditionalInputErrorMessage,
-        }}]}
+        options={[{
+          value: firstValue, title: firstTitle, conditional: {
+            label: label,
+            type: type,
+            validator: conditionalInputValidator,
+            invalidInputErrorMessage: invalidConditionalInputErrorMessage,
+          }
+        }]}
         title={''}
         checked={firstValue}
-        conditionalValue={{[firstValue]: 'bad value'}}
+        conditionalValue={{ [firstValue]: 'bad value' }}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(container.querySelector(`#${fieldSetName}-conditional-error`).textContent).toBe(invalidConditionalInputErrorMessage);
   })
   test('Clicking \'Submit\' button without conditional value shows error summary title', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
     const label = 'Please enter your information';
     const type = 'text';
-    const mockCallBack = jest.fn();
-    const errorSummaryTitle = 'There is a problem';
-    const emptyInputErrorMessage = 'Please enter something';
-
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle, conditional: {
-          label: label,
-          type: type,
-          emptyInputErrorMessage: emptyInputErrorMessage
-        }}]}
+        options={[{
+          value: firstValue, title: firstTitle, conditional: {
+            label: label,
+            type: type,
+            emptyInputErrorMessage: emptyInputErrorMessage
+          }
+        }]}
         title={''}
         checked={firstValue}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
 
     expect(container.querySelector('#error-summary-title').textContent).toBe(errorSummaryTitle);
   })
   test('Clicking \'Submit\' button without selection shows error summary title', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    const mockCallBack = jest.fn();
-    const errorText = 'Required input value missing';
-    const errorSummaryTitle = 'There is a problem';
-
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}]}
+        options={[{ value: firstValue, title: firstTitle }]}
         title={''}
         errorText={errorText}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(container.querySelector('#error-summary-title').textContent).toBe(errorSummaryTitle);
   });
   test('Clicking \'Submit\' button without selection shows error summary text', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
-    const mockCallBack = jest.fn();
-    const errorText = 'Required input value missing';
-
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle}]}
+        options={[{ value: firstValue, title: firstTitle }]}
         title={''}
         errorText={errorText}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
     expect(container.querySelector('#error-summary-text').textContent).toBe(errorText);
   });
   test('Clicking \'Submit\' button without conditional value shows error summary text', () => {
-    const fieldSetName = 'fieldSetName';
-    const firstValue = 'first';
-    const firstTitle = 'First';
     const label = 'Please enter your information';
     const type = 'text';
-    const mockCallBack = jest.fn();
-    const emptyInputErrorMessage = 'Please enter something';
-
     act(() => {
       render(<RadioFieldSet
         onSubmit={mockCallBack}
         name={fieldSetName}
-        options={[{value: firstValue, title: firstTitle, conditional: {
-          label: label,
-          type: type,
-          emptyInputErrorMessage: emptyInputErrorMessage
-        }}]}
+        options={[{
+          value: firstValue, title: firstTitle, conditional: {
+            label: label,
+            type: type,
+            emptyInputErrorMessage: emptyInputErrorMessage
+          }
+        }]}
         title={''}
         checked={firstValue}
       />, container)
       let button = container.querySelector('button');
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}))
+      button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
     });
-
     expect(container.querySelector('#error-summary-text').textContent).toBe(emptyInputErrorMessage);
   });
 })
