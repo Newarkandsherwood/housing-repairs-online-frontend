687efa05f4d3ea5aac12e3c7d4e4e350eb3b58b4
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-08-17 12:10:41 +0100

    93 create unit tests for errorSummary component

--- tests/unit/components/errorSummary.test.js
+++ tests/unit/components/errorSummary.test.js
@@ -4,10 +4,15 @@ import {act} from 'react-dom/test-utils';
 import {render, unmountComponentAtNode} from 'react-dom';
 
 let container = null;
+
 beforeEach(() => {
   // setup a DOM element as a render target
   container = document.createElement('div');
   document.body.appendChild(container);
+
+  act(() => {
+    render(<ErrorSummary/>, container)
+  });
 });
 
 afterEach(() => {
@@ -17,14 +22,20 @@ afterEach(() => {
   container = null;
 });
 
+const errorSummaryTitle = 'There is a problem';
+const errorSummaryDescription = 'Select the problem you are reporting';
+const linkValue = "#errorLink";
+
 describe('errorSummary', () => {
   test('Error summary title should be rendered', () => {
-    const errorSummaryTitle = 'There is a problem';
-
-    act(() => {
-      render(<ErrorSummary/>, container)
-    });
-
     expect(container.querySelector('#error-summary-title').textContent).toBe(errorSummaryTitle);
+  })
+
+  test('Error summary description should be rendered', () => {
+    expect(container.querySelector('#error-summary-text').textContent).toBe(errorSummaryDescription);
+  })
+
+  test('Error summary description link should be rendered', () => {
+    expect(container.querySelector('#error-summary-text').getAttribute('href')).toBe(linkValue)
   })
 })
