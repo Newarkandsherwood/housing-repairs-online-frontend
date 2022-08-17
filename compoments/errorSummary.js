a4997fbb70717b7a8614cb957c75c68b86c34b6f
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-08-17 12:19:21 +0100

    93 create an errorSummary component

--- compoments/errorSummary.js
+++ compoments/errorSummary.js
@@ -1,8 +1,36 @@
-import {Component} from 'react';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 export class ErrorSummary extends Component {
+    constructor(props) {
+        super(props);
+        this.errorSummaryText = this.props.errorSummaryText;
+        this.errorSummaryHref = this.props.errorSummaryHref;
+      }
+
     render() {
-        return <div />
+        return (
+            <div className="govuk-error-summary" aria-labelledby="error-summary-title"
+                role="alert" data-module="govuk-error-summary">
+                <h2 className="govuk-error-summary__title" id="error-summary-title">
+                    There is a problem
+                </h2>
+                <div className="govuk-error-summary__body">
+                    <ul className="govuk-list govuk-error-summary__list">
+                        <li>
+                            <a id="error-summary-text" href="`${this.errorSummaryHref}`" className="govuk-link--no-visited-state">
+                                {this.errorSummaryText}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
     }
 }
+
+ErrorSummary.propTypes = {
+    errorSummaryText: PropTypes.string.isRequired,
+    errorSummaryHref: PropTypes.array.isRequired,
+  };
