011e5f445d4d43a2409282a86773ab9ab1677490
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-08-19 12:37:51 +0100

    93 autofocus ErrorSummary on render

--- compoments/errorSummary.js
+++ compoments/errorSummary.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 
 const ErrorSummary = ({ errorSummaryText, errorSummaryLocation }) => {
+  const inputReference = useRef(null);
+
+  useEffect(() => {
+    inputReference.current.focus();
+  }, []);
+
   return (
     <div className="govuk-error-summary" aria-labelledby="error-summary-title"
-      role="alert" data-module="govuk-error-summary">
+      role="alert" data-module="govuk-error-summary" tabIndex="0" ref={inputReference}>
       <h2 className="govuk-error-summary__title" id="error-summary-title">
         There is a problem
       </h2>
