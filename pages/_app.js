96320b27925968effa19e108b7411014423221aa
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-08-31 09:39:44 +0100

    Add a span that takes focus on reloading the page
    Co-authored-by: CatherineDeas <catherine.deas@madetech.com>

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import '../styles/globals.css';
 import '../styles/globals.scss';
 import React from 'react';
 import App from 'next/app';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import {useRouter} from 'next/router';
 import Header from '../compoments/header';
 import SkipLink from '../compoments/skipLink';
 import Footer from '../compoments/footer';
@@ -13,8 +14,18 @@ function MyApp({ Component, pageProps, err }) {
   };
   useEffect(enableJavascript, []);
 
+  const focusRef = useRef(null);
+
+  const router = useRouter()
+  const currentPath = router.query.route
+
+  useEffect(() => {
+    focusRef.current?.focus();
+  }, [currentPath]);
+
   return (
     <>
+      <span ref={focusRef} tabIndex={-1} />
       <SkipLink linkLocation='main-content' />
       <Header></Header>
       <div className="govuk-width-container">
