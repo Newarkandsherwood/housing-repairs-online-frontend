f93a372e66e17372d31da358b8172ef8cf184c4b
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-09-15 12:34:22 +0100

    Make out of hours emergency number configurable
    Creates a variable in globals.js for the out of hours emergency number

--- compoments/contactNumbers.js
+++ compoments/contactNumbers.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {outOfHoursEmergencyRepairsNumber} from '../globals'
 
 export default function ContactNumbers({ }) {
   return (
     <>
       <p>
-        Emergency Out of Hours Repairs - Telephone: <strong>01522 534747</strong>
+        Emergency Out of Hours Repairs - Telephone: <strong>{outOfHoursEmergencyRepairsNumber}</strong>
       </p>
       <p>
         Emergency In Hours Repairs - Telephone:  <strong>01522 873333</strong>
--- globals.js
+++ globals.js
@@ -1,2 +1,3 @@
 export const emergencyGasServiceNumber = '0800 111 999'
 export const textPhoneMiniComNumber = '0800 371 787'
+export const outOfHoursEmergencyRepairsNumber = '01522 534747'
