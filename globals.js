750e5ad7855cda85276a3dc662f015d4eac66e43
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-09-16 10:44:02 +0100

    Update opening hours with Newark values

--- globals.js
+++ globals.js
@@ -10,8 +10,8 @@ export const OpeningHours = () => {
   return <ul>
     <li>Monday 9am - 5pm</li>
     <li>Tuesday 9am - 5pm</li>
-    <li>Wednesday 10am - 4.30pm</li>
+    <li>Wednesday 9am - 5pm</li>
     <li>Thursday 9am - 5pm</li>
-    <li>Friday 9am - 4.30pm</li>
+    <li>Friday 9am - 5pm</li>
   </ul>
 };
