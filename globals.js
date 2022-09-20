fb0f8da1d5fc30eb5a2de78bbd2e2e26de68c731
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-09-20 15:57:12 +0100

    Make opening hours configurable
    This adds two environment variables for the opening hours.
    
    One allows a list of opening hours.
    The other allows for a simple line for the opening hours.

--- globals.js
+++ globals.js
@@ -4,14 +4,26 @@ export const nationalGasEmergencyServiceTelephoneNumber = process.env.NATIONAL_G
 export const nationalGasEmergencyServiceTextphoneNumber = process.env.NATIONAL_GAS_EMERGENCY_SERVICE_TEXTPHONE_NUMBER
 export const customerServicesTelephoneNumber = process.env.CUSTOMER_SERVICES_TELEPHONE_NUMBER
 export const councilWebsiteHomePageUrl = process.env.COUNCIL_WEBSITE_HOMEPAGE_URL
+export const customer_services_opening_times_full_description = process.env.CUSTOMER_SERVICES_OPENING_TIMES_FULL_DESCRIPTION
+export const customer_services_opening_times_simple_text = process.env.CUSTOMER_SERVICES_OPENING_TIMES_SIMPLIFIED
 
 export const OpeningHours = () => {
+  if( customer_services_opening_times_full_description ) {
+    const openingTmes = JSON.parse(customer_services_opening_times_full_description)
+    const openingHoursList = Object.entries(openingTmes).map(([day, time]) => {
+      return (
+        <li key={day}>{`${day}: ${time}`}</li>
+      );
+    })
 
-  return <ul>
-    <li>Monday 9am - 5pm</li>
-    <li>Tuesday 9am - 5pm</li>
-    <li>Wednesday 9am - 5pm</li>
-    <li>Thursday 9am - 5pm</li>
-    <li>Friday 9am - 5pm</li>
-  </ul>
+    return (
+      <ul>
+        {openingHoursList}
+      </ul>
+    )
+  } else {
+    return (
+      <p>{customer_services_opening_times_simple_text}</p>
+    )
+  }
 };
