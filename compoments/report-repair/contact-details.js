bdccc99628c128c151cd5ebbbee2557d26931200
Renny <32823756+Renny4Real@users.noreply.github.com>
2022-09-23 15:45:19 +0100

    Add an colon before opening hours
    Co-authored-by: Bhavesh-Patel <Bhavesh-Patel@users.noreply.github.com>

--- compoments/report-repair/contact-details.js
+++ compoments/report-repair/contact-details.js
@@ -52,7 +52,7 @@ const ContactDetails = ({handleChange, values}) => {
   const beforeButton =  (
     <Details summary="I have neither a mobile number nor an email address" testid="no-applicable-contact-options-prompt">
       <div data-testid='no-applicable-contact-options-info'>
-        Please call {customerServicesTelephoneNumber} to report your repair during the office hours below
+        Please call {customerServicesTelephoneNumber} to report your repair during the office hours below:
         <OpeningHours />
       </div>
     </Details>
