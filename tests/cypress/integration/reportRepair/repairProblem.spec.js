ef03ef693a4b27c36ea0f4c19459aaf3a89e4d09
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-09-26 10:07:31 +0100

    Loop through the options on repairProblem cypress tests to dynamically create test units

--- tests/cypress/integration/reportRepair/repairProblem.spec.js
+++ tests/cypress/integration/reportRepair/repairProblem.spec.js
@@ -12,24 +12,27 @@ describe('repairProblem', () => {
       makeSelectionAndClickButton('Kitchen');
     });
 
+    const options = [
+      'Cupboards, including damaged cupboard doors',
+      'Electrical, including extractor fans and lightbulbs',
+      'Damaged worktop',
+      'Heating or hot water',
+      'Damaged or stuck doors',
+      'Walls, floor or ceiling, excluding damp',
+      'Sink, including taps and drainage',
+      'Damaged or stuck windows',
+      'Damp or mould'
+    ]
+
     it('displays the repair problem question', () => {
       cy.contains('What is the problem?');
     });
 
-    it('displays all the options', () => {
-      const options = [
-        'Cupboards, including damaged cupboard doors',
-        'Electrical, including extractor fans and lightbulbs',
-        'Damaged worktop',
-        'Heating or hot water',
-        'Damaged or stuck doors',
-        'Walls, floor or ceiling, excluding damp',
-        'Sink, including taps and drainage',
-        'Damaged or stuck windows',
-        'Damp or mould'
-      ]
-      options.forEach((option) => {cy.contains(option)})
-    });
+    options.forEach((option) => {
+      it(`Displays '${option}' option`, () => {
+        cy.contains(option)
+      })
+    })
 
     it('show validation message when a user doesn\'t select anything',  () => {
       cy.get('button').click().then(()=>{
