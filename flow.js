b1caca9a5a53081f970ef7e9b9519caf52b419b3
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-09-28 11:49:53 +0100

    Fix next step for repair-image-upload
    Co-authored-by: Mark Warriner <mark.warriner@madetech.com>

--- flow.js
+++ flow.js
@@ -29,7 +29,7 @@ class Flow {
       'repair-problems': { prevStep: 'repair-location', nextStep: repairTriageNextSteps},
       'repair-problem-best-description': { prevStep: 'repair-problems', nextStep: repairTriageNextSteps},
       'repair-description': {prevStep: true, nextStep: isMvpReleaseVersion()? 'contact-details' : 'repair-image-upload'},
-      'repair-image-upload': { prevStep: 'repair-description', nextStep: 'contact-details'},
+      'repair-image-upload': { prevStep: 'repair-description', nextStep: 'contact-person'},
       'contact-person': {prevStep: 'repair-description', nextStep:'contact-details'},
       'contact-details': {prevStep: 'contact-person', nextStep: 'repair-availability'},
       'repair-availability': {prevStep: 'contact-details', nextStep: 'summary'},
