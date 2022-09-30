ad3af6ced0faa0e820d07ff9c205c88fbdcbd815
CatherineDeas <catherine.deas@madetech.com>
2022-09-30 12:28:51 +0100

    Add 2nd test
    Co-authored-by: Renny <Renny4Real@users.noreply.github.com>

--- tests/cypress/integration/reportRepair/smellGas.spec.js
+++ tests/cypress/integration/reportRepair/smellGas.spec.js
@@ -3,6 +3,13 @@ describe('smellGas', () => {
     cy.visit('http://localhost:3000/report-repair/smell-gas');
   })
   it('Displays non-mvp title', () => {
-    cy.contains('What to do if you smell gas')
+    cy.contains('What to do if you smell gas');
+    cy.get('li').contains('put out all naked flames');
+    cy.get('li').contains('open doors and windows');
+  })
+
+  it('Displays mvp title', () => {
+    cy.contains('If you smell gas');
+    cy.get('p[className="govuk-body-m"]').contains('If you can smell gas, please call the gas emergency number: 0800 111 999');
   })
 })
