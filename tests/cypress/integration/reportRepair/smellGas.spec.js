7e4f977ff396d2bc500cec3f25c6891f344cb82b
CatherineDeas <catherine.deas@madetech.com>
2022-10-03 12:53:21 +0100

    Create separate MVP test file
    Co-authored-by: Renny <Renny4Real@users.noreply.github.com>

--- tests/cypress/integration/reportRepair/smellGas.spec.js
+++ tests/cypress/integration/reportRepair/smellGas.spec.js
@@ -8,8 +8,4 @@ describe('smellGas', () => {
     cy.get('li').contains('open doors and windows');
   })
 
-  it('Displays mvp title', () => {
-    cy.contains('If you smell gas');
-    cy.get('p[className="govuk-body-m"]').contains('If you can smell gas, please call the gas emergency number: 0800 111 999');
-  })
 })
