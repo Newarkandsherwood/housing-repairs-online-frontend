fe4922aa6395020b5f8bc30730e9bbee18865ea1
CatherineDeas <catherine.deas@madetech.com>
2022-10-04 15:24:23 +0100

    Add conditional rendering for MVP
    Co-authored-by: Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
    Co-authored-by: Mark Warriner <104864394+markWarr@users.noreply.github.com>
    Co-authored-by: agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>

--- compoments/report-repair/smell-gas.js
+++ compoments/report-repair/smell-gas.js
@@ -3,35 +3,52 @@ import {serviceName} from '../../helpers/constants';
 import { isMvpReleaseVersion } from '../../helpers/features';
 
 const SmellGas = () => {
-  const title = isMvpReleaseVersion() ? 'If you smell gas': 'What to do if you smell gas';
-  return <div className="govuk-grid-row">
-    <header>
-      <title>{title} - {serviceName}</title>
-    </header>
-    <div className="govuk-grid-column-two-thirds">
-      <h1 className='govuk-heading-xl'>{title}</h1>
-      <div className="govuk-inset-text">
-        If you smell gas you must call the national gas emergency number on <strong>0800 111 999</strong>
+  if (isMvpReleaseVersion()) {
+    const title = 'If you smell gas'
+    return <div className="govuk-grid-row">
+      <header>
+        <title>{title} - {serviceName}</title>
+      </header>
+      <div className="govuk-grid-column-two-thirds">
+        <h1 className='govuk-heading-xl'>{title}</h1>
+        <p className="govuk-body-m">
+          If you can smell gas, please call the gas emergency number: 0800 111 999
+        </p>
       </div>
-      <label className="govuk-label" htmlFor="description">
-        <div>
-          <p>Do not:</p>
-          <ul className="govuk-list govuk-list--bullet">
-            <li>smoke or strike any matches</li>
-            <li>turn electrical equipment or switches on or off</li>
-            <li>interfere with, or repair gas equipment yourself</li>
-          </ul>
-          <p>Do:</p>
-          <ul className="govuk-list govuk-list--bullet">
-            <li>put out all naked flames</li>
-            <li>open doors and windows</li>
-            <li>keep away from the affected areas</li>
-            <li>turn off the gas at the meter, if possible</li>
-          </ul>
-        </div>
-      </label>
     </div>
-  </div>
+  }
+
+  else {
+    const title = 'What to do if you smell gas';
+    return <div className="govuk-grid-row">
+      <header>
+        <title>{title} - {serviceName}</title>
+      </header>
+      <div className="govuk-grid-column-two-thirds">
+        <h1 className='govuk-heading-xl'>{title}</h1>
+        <div className="govuk-inset-text">
+          If you smell gas you must call the national gas emergency number on <strong>0800 111 999</strong>
+        </div>
+        <label className="govuk-label" htmlFor="description">
+          <div>
+            <p>Do not:</p>
+            <ul className="govuk-list govuk-list--bullet">
+              <li>smoke or strike any matches</li>
+              <li>turn electrical equipment or switches on or off</li>
+              <li>interfere with, or repair gas equipment yourself</li>
+            </ul>
+            <p>Do:</p>
+            <ul className="govuk-list govuk-list--bullet">
+              <li>put out all naked flames</li>
+              <li>open doors and windows</li>
+              <li>keep away from the affected areas</li>
+              <li>turn off the gas at the meter, if possible</li>
+            </ul>
+          </div>
+        </label>
+      </div>
+    </div>
+  }
 };
 
 export default SmellGas;
--- tests/cypress/integration/reportRepair/smellGasMvp.spec.js
+++ tests/cypress/integration/reportRepair/smellGasMvp.spec.js
@@ -1,10 +1,14 @@
 describe('smellGas', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/report-repair/smell-gas');
-  })
+  context('conditional mvp tests', () => {
+    if (Cypress.env('CYPRESS_RELEASE_VERSION') == 'mvp') {
+      beforeEach(() => {
+        cy.visit('http://localhost:3000/report-repair/smell-gas');
+      });
 
-  it('Displays mvp title', () => {
-    cy.get('h1[class="govuk-heading-xl"]').contains('If you smell gas');
-/*     cy.get('p[class="govuk-body-m"]').contains('If you can smell gas, please call the gas emergency number: 0800 111 999'); */
-  })
-})
+      it('Displays mvp title', () => {
+        cy.get('h1[class="govuk-heading-xl"]').contains('If you smell gas');
+        cy.get('p[class="govuk-body-m"]').contains('If you can smell gas, please call the gas emergency number: 0800 111 999');
+      });
+    }
+  });
+});
