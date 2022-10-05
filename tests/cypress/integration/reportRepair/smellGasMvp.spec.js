047ca4ec2451dde9bf3e66de17d1384b9c249bb5
CatherineDeas <catherine.deas@madetech.com>
2022-10-05 11:47:27 +0100

    Separate MVP tests

--- tests/cypress/integration/reportRepair/smellGasMvp.spec.js
+++ tests/cypress/integration/reportRepair/smellGasMvp.spec.js
@@ -1,4 +1,4 @@
-describe('smellGas', () => {
+describe('smellGas mvp', () => {
   context('conditional mvp tests', () => {
     if (Cypress.env('CYPRESS_RELEASE_VERSION') == 'mvp') {
       beforeEach(() => {
@@ -7,6 +7,9 @@ describe('smellGas', () => {
 
       it('Displays mvp title', () => {
         cy.get('h1[class="govuk-heading-xl"]').contains('If you smell gas');
+      });
+
+      it('Displays paragraph', () => {
         cy.get('p[class="govuk-body-m"]').contains('If you can smell gas, please call the gas emergency number: 0800 111 999');
       });
     }
