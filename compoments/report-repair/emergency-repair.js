7f9dc61141dd8d092fdf5ad03d0c78af373f5abc
CatherineDeas <catherine.deas@madetech.com>
2022-10-06 17:16:51 +0100

    Create components for one and two phone numbers and updated example.env
    Co-authored-by: agnieszkazbikowska-madetech <agnieszkazbikowska-madetech@users.noreply.github.com>

--- compoments/report-repair/emergency-repair.js
+++ compoments/report-repair/emergency-repair.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ContactNumbers from '../contactNumbers';
+import { customerServicesTelephoneNumber, outOfHoursCustomerServicesTelephoneNumber } from '../../globals'
 import { serviceName } from '../../helpers/constants';
 import TextLink from '../textLink';
 
+  /*   const x = () => {
+      return <><p>If you need an emergency repair (immediate danger to your safety, or that of your Council property) call us immediately.</p><br></br><ContactNumbers /></>
+    }
+
+    const y = () => {
+      return <p>If you need an emergency repair (immediate danger to your safety, or that of your Council property) call us immediately on <strong>{customerServicesTelephoneNumber}</strong></p>
+    } */
+
+function OnePhoneNbr() {
+  return <p>If you need an emergency repair (immediate danger to your safety, or that of your Council property) call us immediately on <strong>{customerServicesTelephoneNumber}</strong></p>;
+}
+function TwoPhoneNbrs() {
+  return <><p>If you need an emergency repair (immediate danger to your safety, or that of your Council property) call us immediately.</p><br></br><ContactNumbers /></>;
+}
 
 const EmergencyRepair = ({ prevStep }) => {
   const title = 'Your repair could be an emergency'
@@ -13,11 +28,16 @@ const EmergencyRepair = ({ prevStep }) => {
     </header>
     <div className="govuk-grid-column-two-thirds">
       <h1 className='govuk-heading-xl'>{title}</h1>
-      <div className="govuk-inset-text">
-        If you need an emergency repair (immediate danger to your safety, or that of your Council property) call us immediately.
-        <br></br>
-        <ContactNumbers />
-      </div>
+
+      {if (outOfHoursCustomerServicesTelephoneNumber) {
+      return <TwoPhoneNbrs />;
+    }
+      return <OnePhoneNbr />;
+  }
+        {/* <div className="govuk-inset-text">
+         {outOfHoursCustomerServicesTelephoneNumber ? x : y}
+       </div> */}
+
       <p className="govuk-body">
         <TextLink href="smell-gas">What to do if you smell gas</TextLink>
       </p>
