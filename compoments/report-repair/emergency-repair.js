1c3997c1b2fe27394cc40c0d944697eda95a096d
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-10-07 10:00:40 +0100

    Keep MVP and full versions of emergency repair pages separate

--- compoments/report-repair/emergency-repair.js
+++ compoments/report-repair/emergency-repair.js
@@ -3,8 +3,42 @@ import ContactNumbers from '../contactNumbers';
 import { customerServicesTelephoneNumber, outOfHoursCustomerServicesTelephoneNumber } from '../../globals'
 import { serviceName } from '../../helpers/constants';
 import TextLink from '../textLink';
+import { OpeningHours } from '../openingHours';
+import { isMvpReleaseVersion } from '../../helpers/features';
 
-const EmergencyRepair = ({ prevStep }) => {
+const EmergencyRepairMvp = () => {
+  const title = 'Your repair could be an emergency'
+
+  return <div className="govuk-grid-row govuk-body-m">
+    <header>
+      <title>{title} - {serviceName}</title>
+    </header>
+    <div className="govuk-grid-column-two-thirds">
+      <h1 className='govuk-heading-xl'>{title}</h1>
+      <h3 className='govuk-heading-m'>Emergencies</h3>
+      <p>
+        An emergency is defined as something which could cause danger to
+        someone’s health or safety or cause serious damage and destruction to
+        property.
+      </p>
+      <ContactNumbers/>
+      <p>
+        Opening times:
+      </p>
+      <OpeningHours />
+      <p>
+        Please do not call the emergency out of hours number if the repair is not urgent.
+      </p>
+      <p>
+        If you can smell gas, you must report it immediately to
+        the Gas Emergency Service <strong>0800 111 999</strong> or via textphone (minicom) on
+        <strong> 0800 371 787</strong>
+      </p>
+    </div>
+  </div>
+};
+
+const EmergencyRepairFull = ({ prevStep }) => {
   const title = 'Your repair could be an emergency'
 
   return <div className="govuk-grid-row govuk-body-m">
@@ -40,6 +74,10 @@ const EmergencyRepair = ({ prevStep }) => {
         </p>}
     </div>
   </div>
+};
+
+const EmergencyRepair = ({ prevStep }) => {
+  return isMvpReleaseVersion() ? <EmergencyRepairMvp /> : <EmergencyRepairFull prevStep={prevStep} />
 };
 
 export default EmergencyRepair;
