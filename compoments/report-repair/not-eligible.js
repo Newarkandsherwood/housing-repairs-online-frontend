32ad690d3787152e5c46dc3940e7636660afdbc9
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-10-11 10:21:26 +0100

    Separate NotEligible component to Mvp and Full

--- compoments/report-repair/not-eligible.js
+++ compoments/report-repair/not-eligible.js
@@ -4,9 +4,51 @@ import ContactNumbers from '../contactNumbers';
 import {customerServicesTelephoneNumber} from '../../globals';
 import {OpeningHours} from '../openingHours';
 import ComponentHeader from '../componentHeader';
+import { isMvpReleaseVersion } from '../../helpers/features';
 
+const NotEligibleMvp = () => {
+  const title = 'The council may not be responsible for repairs at this property'
+  return (
+    <div className="govuk-grid-row govuk-body-m">
+      <header>
+        <title>{title} - {serviceName}</title>
+      </header>
+      <div className="govuk-grid-column-two-thirds">
+        <h1 className='govuk-heading-xl'>{title}</h1>
+        <h3 className='govuk-heading-m'>Emergencies</h3>
+        <p>
+          An emergency is defined as something which could cause danger to
+          someone’s health or safety or cause serious damage and destruction to
+          property.
+        </p>
+        <ContactNumbers/>
+        <Details summary="Opening times" testid="opening-times">
+          <OpeningHours />
+        </Details>
+        <p>
+          Please do not call the emergency out of hours number if the repair is
+          not urgent. We may charge you a fee if the repair is not deemed an
+          emergency.
+        </p>
+        <p>
+          If you suspect you have a gas leak, you must report it immediately to
+          the Gas Emergency Service <strong>0800 111 999</strong> or via textphone (minicom)
+          on <strong>0800 371 787</strong>
+        </p>
+        <hr></hr>
+        <h3>Non-emergency requests</h3>
+        <p>
+          If you think the council is responsible for your property, please call <strong>{customerServicesTelephoneNumber}</strong>.
+        </p>
+        <p>
+          If you are a leaseholder you can still report communal repairs on <strong>{customerServicesTelephoneNumber}</strong>.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-const NotEligible = () => {
+const NotEligibleFull = () => {
   const title = 'The council may not be responsible for repairs at this property'
   return (
     <div className="govuk-grid-row govuk-body-m">
@@ -43,6 +85,12 @@ const NotEligible = () => {
         </p>
       </div>
     </div>
+  );
+};
+
+const NotEligible = () => {
+  return (
+    isMvpReleaseVersion() ? <NotEligibleMvp /> : <NotEligibleFull />
   );
 };
 
