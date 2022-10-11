dda1a5d80c26b27049d03c30c10a653f46d0aed1
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-10-11 10:31:16 +0100

    Add a unit test for ComponentHeader

---
+++ tests/unit/components/componentHeader.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { render, unmountComponentAtNode } from 'react-dom';
+import ComponentHeader from '../../../compoments/ComponentHeader';
+import { serviceName } from '../../../helpers/constants';
+
+let container = null;
+
+const mockTitle = 'Lorem ipslum';
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  act(() => {
+    render(<ComponentHeader title={mockTitle}  />, container)
+  });
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('ComponentHeader', () => {
+
+  test('ComponentHeader text should be rendered', () => {
+    expect(container.querySelector('title').textContent).toBe(`${mockTitle} - ${serviceName}`);
+  })
+})
