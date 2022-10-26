5e2c80fff699b40b8ee1e93426eba600f212561a
Mark Warriner <mark.warriner@madetech.com>
2022-10-26 17:03:19 +0100

    Communal changes for the request summary page

--- compoments/report-repair/summary.js
+++ compoments/report-repair/summary.js
@@ -2,10 +2,12 @@ import React from 'react';
 import SummaryList from '../summaryList';
 import Button from '../button';
 import {isMvpReleaseVersion}  from '../../helpers/features';
+import { getRepairType } from '../../helpers/repairType';
 import ComponentHeader from '../componentHeader';
 
 const Summary = ({values, submit, goToStep}) => {
   const title = 'Request summary'
+  const repairType = getRepairType(values);
 
   const personalDetailsSummary = [
     {pageName:'Repair address', value: values.address?.display, link: 'postcode'},
@@ -14,19 +16,33 @@ const Summary = ({values, submit, goToStep}) => {
   const repairDetailsSummary = [
     { pageName:'Where is the problem?', value: values.repairLocation?.display, link:'repair-location'},
     { pageName:'What is the problem?', value: values.repairProblem?.display, link: 'repair-problems'},
-    { pageName:'What best describes the problem?', value: values['repairProblemBestDescription']?.display, link: 'repair-problem-best-description'},
     { pageName:'Description', value: values.description?.text, link:'repair-description'}
   ]
-  if (!isMvpReleaseVersion()) {
+  if (!isMvpReleaseVersion() && repairType != 'Communal') {
     repairDetailsSummary.push(
       { pageName:'Repair Image', value: values.description?.filename, link:'repair-image-upload'}
     );
+
+    if (repairType != 'Communal') {
+      repairDetailsSummary.push(
+      { pageName:'What best describes the problem?', value: values['repairProblemBestDescription']?.display, link: 'repair-problem-best-description'},
+      )};
   }
 
   const appointmentDetailsSummary = [
     { pageName:'Date', value: values.availability?.display, link:'repair-availability'},
     { pageName:'Confirmation contact details', value: values.contactDetails?.value, link: 'contact-details'},
   ]
+
+  const AppointmentDetailsRender = () => {
+    if (repairType != 'Communal') {
+      return <>
+          <h2 className="govuk-heading-m">Appointment details</h2>
+            <SummaryList goToStep={goToStep} summary={appointmentDetailsSummary}/>
+        </>      
+    }
+    else return '';
+  }
 
   return(
     <>
@@ -40,8 +56,7 @@ const Summary = ({values, submit, goToStep}) => {
               <SummaryList goToStep={goToStep} summary={personalDetailsSummary}/>
               <h2 className="govuk-heading-m">Repair details</h2>
               <SummaryList goToStep={goToStep} summary={repairDetailsSummary}/>
-              <h2 className="govuk-heading-m">Appointment details</h2>
-              <SummaryList goToStep={goToStep} summary={appointmentDetailsSummary}/>
+              <AppointmentDetailsRender />              
             </div>
           </div>
           <Button
