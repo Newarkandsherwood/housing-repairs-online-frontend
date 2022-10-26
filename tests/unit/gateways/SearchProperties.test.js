84fe673c43a67cc55736af7e4b4cc32e319cea19
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-10-26 15:19:52 +0100

    Update gateway to make communal address requests
    Co-authored-by: Agnieszka Zbikowska <agnieszka.zbikowska@madetech.com>

--- tests/unit/gateways/SearchProperties.test.js
+++ tests/unit/gateways/SearchProperties.test.js
@@ -10,11 +10,21 @@ describe('SearchProperties', () => {
       SearchPropertiesGateway = require('../../../api/gateways/SearchPropertiesGateway')(mockGetRequest);
     });
 
-    test('api gets called appropriately', async () => {
-      const result = await SearchPropertiesGateway(postcode);
+    test('api gets called appropriately for tenant addresses', async () => {
+      const result = await SearchPropertiesGateway(postcode, "no");
 
       expect(mockGetRequest).toHaveBeenCalledWith(
-        {uri: `/addresses?postcode=${postcode}`}
+        {uri: `/Addresses/TenantAddresses?postcode=${postcode}`}
+      )
+
+      expect(result).toEqual(dummyData)
+    });
+
+    test('api gets called appropriately for communal addresses', async () => {
+      const result = await SearchPropertiesGateway(postcode, "yes");
+
+      expect(mockGetRequest).toHaveBeenCalledWith(
+        {uri: `/Addresses/CommunalAddresses?postcode=${postcode}`}
       )
 
       expect(result).toEqual(dummyData)
