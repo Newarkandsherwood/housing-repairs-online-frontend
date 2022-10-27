1f97573ef7671500f670723b1491dcc032f6a523
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-10-27 14:17:47 +0100

    Create a new component for SummaryListItem in SummaryList

--- compoments/summaryList.js
+++ compoments/summaryList.js
@@ -1,27 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import LinkPreservingValues from './linkPreservingValues';
 
-export default function SummaryList ({summary, goToStep}) {
+const SummaryListItem = ({goToStep, key, location, name, value}) => {
+  return (
+    <div className="govuk-summary-list__row" key={key}>
+      <dt className="govuk-summary-list__key">
+        {name}
+      </dt>
+      <dd className="govuk-summary-list__value">
+        {value}
+      </dd>
+      <dd className="govuk-summary-list__actions">
+        <LinkPreservingValues
+          currentLocation ='summary'
+          goToLocation={location}
+          goToStep={goToStep}
+          text='Change'
+        />
+        <span className="govuk-visually-hidden">{name}</span>
+      </dd>
+    </div>
+  )
+}
+
+SummaryListItem.propTypes = {
+  goToStep: PropTypes.func.isRequired,
+  key: PropTypes.number.isRequired,
+  location: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired
+}
+
+export default function SummaryList ({goToStep, summary}) {
 
   return(<dl className="govuk-summary-list">
     {summary.map((o, i) => (
-      <div className="govuk-summary-list__row" key={i}>
-        <dt className="govuk-summary-list__key">
-          {o.pageName}
-        </dt>
-        <dd className="govuk-summary-list__value">
-          {o.value}
-        </dd>
-        <dd className="govuk-summary-list__actions">
-          <LinkPreservingValues
-            currentLocation ='summary'
-            goToLocation={o.link}
-            goToStep={goToStep}
-            text='Change'
-          />
-          <span className="govuk-visually-hidden">{o.pageName}</span>
-        </dd>
-      </div>
+      <SummaryListItem
+        key={i}
+        goToStep={goToStep}
+        name={o.pageName}
+        value={o.value}
+        location={o.link}
+      />
     ))}
   </dl>)
+}
+
+SummaryList.propTypes = {
+  goToStep: PropTypes.func.isRequired,
+  summary: PropTypes.array.isRequired
 }
