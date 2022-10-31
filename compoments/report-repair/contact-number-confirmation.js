584fff99915dcb0e0eccd1d99faf7200e22cdfc3
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-10-31 16:09:58 +0000

    Display contactPersonNumber value on the ContactNumberConfirmation page

--- compoments/report-repair/contact-number-confirmation.js
+++ compoments/report-repair/contact-number-confirmation.js
@@ -7,7 +7,7 @@ import ComponentHeader from '../componentHeader';
 import { conditionalPhoneNumber } from './contact-details';
 
 const ContactNumberConfirmation = ({handleChange, values}) => {
-  const title = `Can we call ${values.contactDetails?.value} if we need to get in touch?`
+  const title = `Can we call ${values.contactPersonNumber} if we need to get in touch?`
   const name = 'ContactNumberConfirmation'
   const Continue = val => {
     handleChange('contactPersonNumber', val.input);
