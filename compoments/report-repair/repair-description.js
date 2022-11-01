979807da5edcf6acf43a6b297bf6553d2684ae26
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-11-01 09:17:37 +0000

    Extract component to own file and renamed to more representative name

--- compoments/report-repair/repair-description.js
+++ compoments/report-repair/repair-description.js
@@ -7,52 +7,7 @@ import ErrorSummary from '../errorSummary';
 import { isMvpReleaseVersion } from '../../helpers/features';
 import ImagePreview from '../image-preview';
 import ComponentHeader from '../componentHeader';
-
-const CharacterCount = ({ errorText, hasExceededTextLimit, onChange, repairDescriptionTextInputId, text, textAreaCount, textLimit }) => {
-
-  const generateCharacterCountText = () => {
-    const characterCountDifference = textLimit - textAreaCount;
-    const absoluteCharacterCountDifference = `${Math.abs(characterCountDifference)}`;
-    const suffix = `${characterCountDifference < 0 ? 'too many' : 'remaining'}`;
-    const characterWord = `character${absoluteCharacterCountDifference == 1 ? '' : 's'}`;
-    return `You have ${absoluteCharacterCountDifference} ${characterWord} ${suffix}`
-  }
-
-  return (
-    <div className='govuk-character-count'>
-      <div className={errorText ? 'govuk-form-group--error' : 'govuk-form-group'}>
-        <label className="govuk-label govuk-label--m" htmlFor="description">
-          Description of problem
-        </label>
-        <span id={'description-error'}
-          className="govuk-error-message">
-          {errorText}
-        </span>
-        <textarea
-          className={`govuk-textarea ${errorText && 'govuk-textarea--error'}`}
-          id={repairDescriptionTextInputId}
-          name="description" type="text"
-          onChange={(e) => onChange(e)}
-          defaultValue={text}
-          rows="5"></textarea>
-        <div id="with-hint-info"
-          className={`${hasExceededTextLimit ? 'govuk-error-message' : 'govuk-hint'} govuk-character-count__message`}
-          aria-live="polite">{generateCharacterCountText()}
-        </div>
-      </div>
-    </div>
-  )
-}
-
-CharacterCount.propTypes = {
-  errorText: PropTypes.string,
-  hasExceededTextLimit: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired,
-  repairDescriptionTextInputId: PropTypes.string.isRequired,
-  text: PropTypes.string,
-  textAreaCount: PropTypes.number.isRequired,
-  textLimit: PropTypes.number.isRequired,
-}
+import LabelledTextareaWithCharacterCount from '../labelledTextareaWithCharacterCount'
 
 const RepairDescription = ({ handleChange, values }) => {
   const [error, setError] = useState({ text: undefined, img: undefined });
@@ -195,7 +150,7 @@ const RepairDescription = ({ handleChange, values }) => {
             </div>
           </div>
         </label>
-        <CharacterCount
+        <LabelledTextareaWithCharacterCount
           errorText={error.text}
           hasExceededTextLimit={textLimit - textAreaCount < 0}
           onChange={TextChange}
