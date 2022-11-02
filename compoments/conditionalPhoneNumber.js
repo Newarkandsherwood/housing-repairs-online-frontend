cabfb8f29fee777339026b257f07fa0052eb0ddd
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-11-02 10:06:50 +0000

    Move conditionalPhoneNumber to a new file

---
+++ compoments/conditionalPhoneNumber.js
@@ -0,0 +1,9 @@
+export const conditionalPhoneNumber = {
+  autoComplete: 'tel',
+  label: 'UK mobile number',
+  type: 'tel',
+  validator: mobilePhoneNumberValidator,
+  onKeyPress: phoneOnKeyPress,
+  emptyInputErrorMessage: 'Enter a UK mobile number',
+  invalidInputErrorMessage: 'Enter a valid UK mobile number',
+}
