aa1d188fb90314f8d781f89e32464f4aa81c32b9
Mark Warriner <mark.warriner@madetech.com>
2022-11-04 11:22:26 +0000

    Add functions to check for repair type and remove an MVP check.

--- helpers/repairType.js
+++ helpers/repairType.js
@@ -1,11 +1,23 @@
+const communalRepairType =  'Communal';
+const tenantRepairType =  'Tenant';
+const leaseholdRepairType =  'Leasehold';
+
 export function getRepairType(data) {
-  var result = 'Tenant';
+  var result = tenantRepairType;
   if(data['communal'] === 'yes'){
-    result = 'Communal'
+    result = communalRepairType
   } else{
     if(data['isTenant'] === 'no' || undefined){
-      result = 'Leasehold'
+      result = leaseholdRepairType
     }
   }
   return result;
+}
+
+export function isCommunalRepairType(data) {
+  return getRepairType(data) === communalRepairType
+}
+
+export function isTenantRepairType(data) {
+  return getRepairType(data) === tenantRepairType
 }
