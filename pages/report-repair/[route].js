190da5a676c1edd1a417e986b1f6577097191a3e
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-11-09 10:54:23 +0000

    Do not offer appointment availability for communal repairs

--- pages/report-repair/[route].js
+++ pages/report-repair/[route].js
@@ -32,7 +32,7 @@ import { fetcher } from '../../helpers/fetcher';
 import ContactUs from '../../compoments/report-repair/contact-us';
 import {customerServicesTelephoneNumber} from '../../globals'
 import TenantOrLeaseholder from '../../compoments/report-repair/tenant-or-leaseholder';
-import { getRepairType } from '../../helpers/repairType';
+import { getRepairType, isCommunalRepairType } from '../../helpers/repairType';
 import ContactNumberConfirmation from '../../compoments/report-repair/contact-number-confirmation';
 
 const ReportRepairWrapper = ({children, prevStep, showBackLink}) => {
@@ -362,6 +362,8 @@ function ReportRepair() {
         />
       )
     case 'repair-image-upload':
+      const imageUploadNextStep = isCommunalRepairType(values) ? 'contact-details' : 'repair-availability';
+      flow = new Flow(setState, router, 'report-repair', prevSteps, setPrevSteps, imageUploadNextStep);
       return (
         <RepairImageUpload
           handleChange={handleChange}
