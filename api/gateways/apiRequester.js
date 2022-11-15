02b5d24f411de0359402151bdce426e513485d8b
Mark Warriner <mark.warriner@madetech.com>
2022-11-15 18:22:10 +0000

    Set repairs API config in pipeline and remove extra debug

--- api/gateways/apiRequester.js
+++ api/gateways/apiRequester.js
@@ -3,12 +3,10 @@ require('dotenv').config()
 module.exports = axios => {
   return {
     makeGetRequest: ({uri, params ={}}) =>{
-      var identifier = process.env.NODE_ENV === 'production' ? process.env.REPAIRS_API_IDENTIFIER : process.env.REPAIRS_API_IDENTIFIER_STAGING
-      var baseUrl = process.env.NODE_ENV === 'production' ? process.env.REPAIRS_API_BASE_URL: process.env.REPAIRS_API_BASE_URL_STAGING;
       const axiosInstance = axios.create({
-        baseURL: baseUrl
+        baseURL: process.env.REPAIRS_API_BASE_URL
       })
-      return axiosInstance.post(`/authentication?identifier=${identifier}`)
+      return axiosInstance.post(`/authentication?identifier=${process.env.REPAIRS_API_IDENTIFIER}`)
         .then(response => {
           var jwt = response.data;
           axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
@@ -19,12 +17,10 @@ module.exports = axios => {
     },
 
     makePostRequest: ({uri, body ={}}) =>{
-      var identifier = process.env.NODE_ENV === 'production' ? process.env.REPAIRS_API_IDENTIFIER : process.env.REPAIRS_API_IDENTIFIER_STAGING
-      var baseUrl = process.env.NODE_ENV === 'production' ? process.env.REPAIRS_API_BASE_URL: process.env.REPAIRS_API_BASE_URL_STAGING;
       const axiosInstance = axios.create({
-        baseURL: baseUrl
+        baseURL: process.env.REPAIRS_API_BASE_URL
       })
-      return axiosInstance.post(`/authentication?identifier=${identifier}`)
+      return axiosInstance.post(`/authentication?identifier=${process.env.REPAIRS_API_IDENTIFIER}`)
         .then(response => {
           var jwt = response.data;
           axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
