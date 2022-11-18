f5c4b36d8858796a55e96376226f14ff1011c882
Mark Warriner <mark.warriner@madetech.com>
2022-11-18 14:37:14 +0000

    Show/hide appointment change link for communal and tenant

--- compoments/report-repair/confirmation.js
+++ compoments/report-repair/confirmation.js
@@ -1,11 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import TextLink from '../textLink';
+import { isCommunalRepairType } from '../../helpers/repairType';
 import ComponentHeader from '../componentHeader';
 
 
-const Confirmation = ({ requestId, confirmation }) => {
+const Confirmation = ({ requestId, confirmation, values }) => {
   const title = 'Repair request complete'
+  const isCommunal = isCommunalRepairType(values);
+
+  const AppointmentChangeRender = () => {
+    if (!isCommunal) {
+      return <p className="govuk-body">
+      <TextLink href="find-appointment">
+        View, change or cancel your appointment
+      </TextLink>
+    </p>  
+    }
+    else return '';
+  }
+
   return (
     <div className="govuk-grid-row" data-cy="confirmation">
       <ComponentHeader title={title} />
@@ -21,7 +35,7 @@ const Confirmation = ({ requestId, confirmation }) => {
           You will need to provide your repair number and
           postcode to either change or cancel your booking.
         </p>
-
+        <AppointmentChangeRender /> 
         <h2 className="govuk-heading-m">What happens next</h2>
         <p>
           We will assess your repair and may be in touch to ask follow-up
--- tests/cypress/integration/reportRepair/confirmation.spec.js
+++ tests/cypress/integration/reportRepair/confirmation.spec.js
@@ -12,6 +12,60 @@ const email = 'harrypotter@hogwarts.com';
 const repairID = '1234ABC'
 
 describe('confirmation', () => {
+  describe('communal confirmation', {
+    env: {
+      REPAIR_TYPE: "communal",
+    },
+  }, () => {
+    before(() => {
+      intercept_availability_search();
+      intercept_address_search();
+      intercept_repair_triage();
+      intercept_save_repair(repairID);
+      completeJourneyUsingEmail(email);
+    });
+
+    it('Displays repair id', () => {
+      cy.get('.govuk-panel').contains('Repair request complete');
+      cy.get('.govuk-panel').contains(repairID);
+    });
+
+    it('Does not display appointment link', () => {
+      cy.get('a[href*="appointment"]').should('not.exist')
+    });
+
+    it('Does not display the back button', () => {
+      cy.get('.govuk-back-link').should('not.exist')
+    });
+
+    context('when user sends confirmation via email', () => {
+      it('Displays where the confirmation was sent to', () => {
+        cy.contains('We have sent a confirmation to ' + email);
+      });
+    });
+
+    it('Displays report another issue link', () => {
+      cy.get('a[href="/"').contains('Report another issue').should('have.attr', 'href', '/');
+    });
+
+    context('when user sends confirmation via text', () => {
+      before(() => {
+        intercept_availability_search();
+        intercept_address_search();
+        intercept_repair_triage();
+        intercept_save_repair(repairID);
+        completeJourneyUsingPhone(phoneNumber);
+      });
+
+      it('Displays where the confirmation was sent to', () => {
+        cy.contains('We have sent a confirmation to ' + phoneNumber);
+      });
+    });
+  });
+
+});
+
+describe('tenant confirmation', () => {
   before(() => {
     intercept_availability_search();
     intercept_address_search();
@@ -25,21 +79,25 @@ describe('confirmation', () => {
     cy.get('.govuk-panel').contains(repairID);
   });
 
-  it('Displays report another issue link', () => {
-    cy.get('a').contains('Report another issue').should('have.attr', 'href', '/');
+  it('Displays appointment link', () => {
+    cy.get('a[href*="appointment"]').contains('View, change or cancel your appointment').should('have.attr', 'href', 'find-appointment');
   });
 
   it('Does not display the back button', () => {
     cy.get('.govuk-back-link').should('not.exist')
   });
 
-  context('when user sends confirmation via email', ()=>{
+  context('when user sends confirmation via email', () => {
     it('Displays where the confirmation was sent to', () => {
       cy.contains('We have sent a confirmation to ' + email);
     });
   });
 
-  context('when user sends confirmation via text', ()=>{
+  it('Displays report another issue link', () => {
+    cy.get('a').contains('Report another issue').should('have.attr', 'href', '/');
+  });
+
+  context('when user sends confirmation via text', () => {
     before(() => {
       intercept_availability_search();
       intercept_address_search();
@@ -52,5 +110,4 @@ describe('confirmation', () => {
       cy.contains('We have sent a confirmation to ' + phoneNumber);
     });
   });
-
 });
