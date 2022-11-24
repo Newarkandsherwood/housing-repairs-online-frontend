70fd3df2dd1d8b5fb9004f00d3a9f44871c56dcf
Mark Warriner <mark.warriner@madetech.com>
2022-11-24 14:43:29 +0000

    Test updates and fixes

--- tests/cypress/integration/reportRepair/confirmation.spec.js
+++ tests/cypress/integration/reportRepair/confirmation.spec.js
@@ -29,96 +29,100 @@ describe('confirmation', () => {
       REPAIR_TYPE: "tenant",
     },
   }, () => {
-    beforeEach(() => {
-      intercept_availability_search();
-      intercept_address_search();
-      intercept_repair_triage();
-      intercept_save_repair(repairResponse);
-      completeJourneyUsingEmail(email);
-    });
-
-    it('Displays repair id', () => {
-      cy.get('.govuk-panel').contains('Repair request complete');
-      cy.get('.govuk-panel').contains(repairId);
-    });
-
-    it('Displays appointment link', () => {
-      cy.get('a[href*="appointment"]').contains('View, change or cancel your appointment').should('have.attr', 'href', 'find-appointment');
-    });
-
-    it('Does not display the back button', () => {
-      cy.get('.govuk-back-link').should('not.exist')
-    });
 
     context('when user sends confirmation via email', () => {
-      it('Displays where the confirmation was sent to', () => {
-        cy.contains('We have sent a confirmation to ' + email);
-      });
-    });
-
-    it('Displays report another issue link', () => {
-      cy.get('a').contains('Report another issue').should('have.attr', 'href', "/");
-    });
-
-    it('Displays assessment message', () => {
-      cy.contains('We will assess your repair and may be in touch to ask follow-up questions');
-    });
-
-    context('when user sends confirmation via phone number', () => {
-      before(() => {
+      beforeEach(() => {
         intercept_availability_search();
         intercept_address_search();
         intercept_repair_triage();
         intercept_save_repair(repairResponse);
-        completeJourneyUsingPhone(phoneNumber);
+        completeJourneyUsingEmail(email);
       });
 
-      it('Displays where the confirmation was sent to', () => {
-        cy.contains('We have sent a confirmation to ' + phoneNumber);
+      it('Displays repair id', () => {
+        cy.get('.govuk-panel').contains('Repair request complete');
+        cy.get('.govuk-panel').contains(repairId);
+      });
+
+      it('Displays appointment link', () => {
+        cy.get('a[href*="appointment"]').contains('View, change or cancel your appointment').should('have.attr', 'href', 'find-appointment');
+      });
+
+      it('Does not display the back button', () => {
+        cy.get('.govuk-back-link').should('not.exist')
+      });
+
+      context('when user sends confirmation via email', () => {
+        it('Displays where the confirmation was sent to', () => {
+          cy.contains('We have sent a confirmation to ' + email);
+        });
+      });
+
+      it('Displays report another issue link', () => {
+        cy.contains('a', 'Report another issue')
+      });
+
+      it('Displays assessment message', () => {
+        cy.contains('We will assess your repair and may be in touch to ask follow-up questions');
       });
     });
-  });
+    context('when user sends confirmation via phone number', () => {
+      context('when user sends confirmation via email', () => {
 
-  describe('communal confirmation', {
-    env: {
-      REPAIR_TYPE: "communal",
-    },
-  }, () => {
-    beforeEach(() => {
-      intercept_availability_search();
-      intercept_address_search();
-      intercept_repair_triage();
-      intercept_save_repair(repairResponseCommunal);
-      completeJourneyUsingEmail(email);
-    });
+        beforeEach(() => {
+          intercept_availability_search();
+          intercept_address_search();
+          intercept_repair_triage();
+          intercept_save_repair(repairResponse);
+          completeJourneyUsingPhone(phoneNumber);
+        });
 
-    it('Displays repair id', () => {
-      cy.get('.govuk-panel').contains('Repair request complete');
-      cy.get('.govuk-panel').contains(repairId);
-    });
-
-    it('Does not display appointment link', () => {
-      cy.get('a[href*="appointment"]').should('not.exist')
-    });
-
-    it('Does not display the back button', () => {
-      cy.get('.govuk-back-link').should('not.exist')
-    });
-
-    context('when user sends confirmation via email', () => {
-      it('Displays where the confirmation was sent to', () => {
-        cy.contains('We have sent a confirmation to ' + email);
+        it('Displays where the confirmation was sent to', () => {
+          cy.contains('We have sent a confirmation to ' + phoneNumber);
+        });
       });
     });
 
-    it('Displays report another issue link', () => {
-      cy.get('a[href="/"').contains('Report another issue').should('have.attr', 'href', '/');
-    });
+    describe('communal confirmation', {
+      env: {
+        REPAIR_TYPE: "communal",
+      },
+    }, () => {
+      beforeEach(() => {
+        intercept_availability_search();
+        intercept_address_search();
+        intercept_repair_triage();
+        intercept_save_repair(repairResponseCommunal);
+        completeJourneyUsingEmail(email);
+      });
 
-    it('Displays assessment message with number of days', () => {
-      cy.contains(`We will assess your request and a repair will be scheduled within the next ${daysForRepair} days`);
-    });
+      it('Displays repair id', () => {
+        cy.get('.govuk-panel').contains('Repair request complete');
+        cy.get('.govuk-panel').contains(repairId);
+      });
 
+      it('Does not display appointment link', () => {
+        cy.get('a[href*="appointment"]').should('not.exist')
+      });
+
+      it('Does not display the back button', () => {
+        cy.get('.govuk-back-link').should('not.exist')
+      });
+
+      context('when user sends confirmation via email', () => {
+        it('Displays where the confirmation was sent to', () => {
+          cy.contains('We have sent a confirmation to ' + email);
+        });
+      });
+
+      it('Displays report another issue link', () => {
+        cy.get('a[href="/"').contains('Report another issue').should('have.attr', 'href', '/');
+      });
+
+      it('Displays assessment message with number of days', () => {
+        cy.contains(`We will assess your request and a repair will be scheduled within the next ${daysForRepair} days`);
+      });
+    });
     context('when user sends confirmation via phone', () => {
       beforeEach(() => {
         intercept_availability_search();
