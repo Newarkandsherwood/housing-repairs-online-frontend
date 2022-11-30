0e533a0f53a5b8fd0f4cb80e0a8c5643a91fe04c
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-11-30 14:24:00 +0000

    Add tests for no repair found
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- compoments/report-repair/repair-appointment.js
+++ compoments/report-repair/repair-appointment.js
@@ -7,6 +7,7 @@ import {fetcher} from '../../helpers/fetcher';
 import Loader from '../loader';
 import Error from "../error";
 import {customerServicesTelephoneNumber} from '../../globals';
+import RepairAppointmentNotFound from './repair-appointment-not-found';
 
 
 const RepairAppointment = ({ handleChange, values}) => {
@@ -24,56 +25,57 @@ const RepairAppointment = ({ handleChange, values}) => {
   const title = 'Your repair appointment'
   return (
     <div className="govuk-grid-row" data-cy={`${name}`}>
-      <ComponentHeader data-cy='repair-appointment-heading' title={title} />
-      <div className="govuk-grid-column-two-thirds">
-        <h1 className='govuk-heading-xl'>{title}</h1>
-        {data &&
-        <>
-          <table className="govuk-table">
-            <thead className="govuk-table__head">
-              <tr className="govuk-table__row">
-                <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-1`}>Repair Address</th>
-                <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-2`}>Date and Time</th>
-                <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-3`}>Area</th>
-                <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-4`}>Type</th>
-              </tr>
-            </thead>
-            <tbody className="govuk-table__body">
-              <tr className="govuk-table__row">
-                <td className="govuk-table__cell" data-cy={`${name}-address`}>{data.address.display}</td>
-                <td className="govuk-table__cell" data-cy={`${name}-date-time`}>{data.appointmentTime.display}</td>
-                <td className="govuk-table__cell" data-cy={`${name}-location`}>{data.location.display}</td>
-                <td className="govuk-table__cell" data-cy={`${name}-problem`}>{data.problem.display}</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="govuk-form-group">
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
-                What would you like to do?
-              </legend>
-              <div className="govuk-radios" data-module="govuk-radios">
-                <div className="govuk-radios__item">
-                  <input className="govuk-radios__input" id={`${name}-change-appointment-input`} data-cy={`${name}-change-appointment-input`} name="[change-repair]" type="radio" value={'Change the time slot of the repair appointment'}>
-                  </input>
-                  <label className="govuk-label govuk-radios__label" htmlFor={`${name}-change-appointment-input`} data-cy={`${name}-change-appointment-label`}>
-                    Change the time slot of the repair appointment
-                  </label>
+      {Object.keys(data).length === 0 && <RepairAppointmentNotFound />}
+      {Object.keys(data).length > 0 && <>
+        <ComponentHeader data-cy='repair-appointment-heading' title={title} />
+        <div className="govuk-grid-column-two-thirds">
+          <h1 className='govuk-heading-xl'>{title}</h1>
+          <>
+            <table className="govuk-table">
+              <thead className="govuk-table__head">
+                <tr className="govuk-table__row">
+                  <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-1`}>Repair Address</th>
+                  <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-2`}>Date and Time</th>
+                  <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-3`}>Area</th>
+                  <th scope="col" className="govuk-table__header app-custom-class" data-cy={`${name}-table-heading-4`}>Type</th>
+                </tr>
+              </thead>
+              <tbody className="govuk-table__body">
+                <tr className="govuk-table__row">
+                  <td className="govuk-table__cell" data-cy={`${name}-address`}>{data.address.display}</td>
+                  <td className="govuk-table__cell" data-cy={`${name}-date-time`}>{data.appointmentTime.display}</td>
+                  <td className="govuk-table__cell" data-cy={`${name}-location`}>{data.location.display}</td>
+                  <td className="govuk-table__cell" data-cy={`${name}-problem`}>{data.problem.display}</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="govuk-form-group">
+              <fieldset className="govuk-fieldset">
+                <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
+                  What would you like to do?
+                </legend>
+                <div className="govuk-radios" data-module="govuk-radios">
+                  <div className="govuk-radios__item">
+                    <input className="govuk-radios__input" id={`${name}-change-appointment-input`} data-cy={`${name}-change-appointment-input`} name="[change-repair]" type="radio" value={'Change the time slot of the repair appointment'}>
+                    </input>
+                    <label className="govuk-label govuk-radios__label" htmlFor={`${name}-change-appointment-input`} data-cy={`${name}-change-appointment-label`}>
+                      Change the time slot of the repair appointment
+                    </label>
+                  </div>
+                  <div className="govuk-radios__item">
+                    <input className="govuk-radios__input" id={`${name}-cancel-appointment-input`} data-cy={`${name}-cancel-appointment-input`} name="[change-repair]" type="radio" value={'Cancel the repair appointment'}>
+                    </input>
+                    <label className="govuk-label govuk-radios__label" htmlFor={`${name}-cancel-appointment-input`} data-cy={`${name}-cancel-appointment-label`}>
+                      Cancel the repair appointment
+                    </label>
+                  </div>
                 </div>
-                <div className="govuk-radios__item">
-                  <input className="govuk-radios__input" id={`${name}-cancel-appointment-input`} data-cy={`${name}-cancel-appointment-input`} name="[change-repair]" type="radio" value={'Cancel the repair appointment'}>
-                  </input>
-                  <label className="govuk-label govuk-radios__label" htmlFor={`${name}-cancel-appointment-input`} data-cy={`${name}-cancel-appointment-label`}>
-                    Cancel the repair appointment
-                  </label>
-                </div>
-              </div>
-            </fieldset>
-          </div>
-        </>
-        }
-        <Button onClick={Continue} >Continue</Button>
-      </div>
+              </fieldset>
+            </div>
+          </>
+          <Button onClick={Continue} >Continue</Button>
+        </div>
+      </>}
     </div>
   );
 };
